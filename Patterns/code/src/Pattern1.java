26d22e4f85554bde77da37d9dd96433110c849e7
Pavan Varma <pavanvarma058@gmail.com>
2025-07-04 11:46:15 +0530

    Modified Pattern 1 and added the solution

--- Patterns/code/src/Pattern1.java
+++ Patterns/code/src/Pattern1.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Pattern1 {
     public static void main(String[] args) {
-        
+        // Pattern-1
+        // input - 5
+        /*
+         *****
+         *****
+         *****
+         *****
+         *****
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
